a58b9bb89913d3b10fbabe2267cb56cf1c7050e9
Silasaraujo <77333741+Silovisk@users.noreply.github.com>
2023-05-21 13:38:53 -0300

    Initial commit

---
+++ flask_app.py
@@ -0,0 +1,20 @@
+import os
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    api_key = os.environ.get('GIPHY_API_KEY')
+    # Busca um GIF aleatório na API do Giphy
+    response = requests.get(f'https://api.giphy.com/v1/gifs/random?api_key=Ib6itq6T3gUkAP6oqDcCKonwCXbdhAYZ&tag=&rating=g')
+    if response.status_code == 200:
+        gif_url = response.json()['data']['images']['original']['url']
+        return render_template('index.html', background_image=gif_url)
+    else:
+        # Trata falha na chamada à API
+        return render_template('error.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
